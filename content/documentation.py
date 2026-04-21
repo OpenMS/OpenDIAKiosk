f1da05baf00ad5cd4406791318d52145b02203a2
Tom David Müller <57191390+t0mdavid-m@users.noreply.github.com>
2026-04-21 10:07:54 +0200

    Revert "Add kubernetes deployment"

--- content/documentation.py
+++ content/documentation.py
@@ -17,7 +17,6 @@ pages = [
     "Developers Guide: TOPP Workflow Framework",
     "Developer Guide: Windows Executables",
     "Developers Guide: Deployment",
-    "Developers Guide: Kubernetes Deployment",
 ]
 page = cols[0].selectbox(
     "**Content**",
@@ -107,13 +106,4 @@ This guide explains how to package OpenMS apps into Windows executables using tw
 if page == pages[5]:
     with open(Path("docs", "deployment.md"), "r", encoding="utf-8") as f:
         content = f.read()
-    st.markdown(content)
-
-#############################################################################################
-# Kubernetes Deployment
-#############################################################################################
-
-if page == pages[6]:
-    with open(Path("docs", "kubernetes-deployment.md"), "r", encoding="utf-8") as f:
-        content = f.read()
-    st.markdown(content)
+    st.markdown(content) 
